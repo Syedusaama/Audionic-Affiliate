e8f71ab04f990d529f43d2e44c97e21e0fcb6857
Syed Usama <syedusama875@gmail.com>
2023-06-06 01:30:59 -0700

    update

--- index.php
+++ index.php
@@ -10,11 +10,111 @@
 
 <body>
     <?php include("includes/header.php"); ?>
-    <div class="mainBanner" style="background-image:url(assets/images/banner/bg-banner.jpg); ">
-        <div class="container">
 
+    <!-- main-banner -->
+<section class="main-banner" style="background-image: url(assets/images/main-banner.png);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-banner-box">
+                    <p>Sed ut perspiciatis unde</p>
+                    <h1>Become An Affiliate!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud .</p>
+                    <a class="main-btn" href="#">Become An Affiliate!</a>
+                </div>
+            </div>
         </div>
     </div>
+</section>
+    <!-- main-banner -->
+
+
+
+<!---------------- Join Our Affiliate Program ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- Join Our Affiliate Program ------------------>
+
+
+
+
+<!---------------- Earn with us at Exceptional Commission Tiers ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- Earn with us at Exceptional Commission Tiers ------------------>
+
+
+
+
+<!---------------- Join  ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- Join  ------------------>
+
+
+
+
+
+<!---------------- How Affiliate Works  ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- How Affiliate Works  ------------------>
+
+
+
+<!---------------- Join Our Affiliate Program  ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- Join Our Affiliate Program  ------------------>
+
+
+
+<!---------------- Frequently Asked Questions ------------------>
+<section class="main-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!---------------- Frequently Asked Questions ------------------>
 
     <?php include("includes/footer.php"); ?>
     <?php include("includes/scripts.php"); ?>
